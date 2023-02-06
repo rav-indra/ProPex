92d5e87055b77402365adc01bca409546213b9bc
ravindra <ravindra.essence@gmail.com>
2023-02-06 11:06:21 +0530

    Changed form error,code of dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,13 @@
 class DashboardController < ApplicationController
   def index
     @users = User.all
-    @props = Property.where(user_id: current_user.id)
-    @sold_property =  @props.where(is_paid: true)
+    @user_properties = current_user.properties
+    @sold_property =  @user_properties.where(is_paid: true)
     @buyer_props = Property.where(buyer_id: current_user.id)
   end 
   # sold properties page
   def sold
-    @props = Property.where(user_id: current_user.id)
-    @sold_property =  @props.where(is_paid: true)
+    @user_properties = current_user.properties
+    @sold_property =  @user_properties.where(is_paid: true)
   end
 end
